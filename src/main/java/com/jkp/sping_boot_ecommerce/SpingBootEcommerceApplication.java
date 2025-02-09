4c575ecee49287dc47bec2ea75b91425429e500d
Theerapol-khamwan <theerapol.khamwan@gmail.com>
2025-02-10 00:23:00 +0700

    setup project

---
+++ src/main/java/com/jkp/sping_boot_ecommerce/SpingBootEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.jkp.sping_boot_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBootEcommerceApplication.class, args);
+	}
+
+}
